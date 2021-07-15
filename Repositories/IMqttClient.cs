b3253b23212e253fb01aad776d2366164c8ecf55
Javid izadi <javid.izadi1384@gmail.com>
2021-07-15 23:56:26 +0430

    Create Concept Interface

---
+++ Repositories/IMqttClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adafruit_MQTT_Client.Repositories
+{
+    public interface IMqttClient
+    {
+        public void ConnectAsync();
+
+        public void Disconnect();
+
+        public void Publish();
+
+        public void Subscribe();
+
+        public void UnSubscribe();
+    }
+}
