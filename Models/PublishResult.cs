4a0eb063b06275783b0e82dffac22915928de661
Javid izadi <javid.izadi1384@gmail.com>
2021-07-18 14:57:49 +0430

    Add "PublishResult" Model

---
+++ Models/PublishResult.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adafruit_MQTT_Client.Models
+{
+    public class PublishResult : MQTTnet.Client.Publishing.MqttClientPublishResult { }
+}
