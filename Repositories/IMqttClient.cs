2d9bc4cffecf8cc5debaf3b1eb8da68f958220ea
javid.izadi <javid.izadi1384@gmail.com>
2021-09-01 18:05:05 +0430

    Update Main Interface

--- Repositories/IMqttClient.cs
+++ Repositories/IMqttClient.cs
@@ -14,35 +14,42 @@ namespace Adafruit.MQTT.Client.Repositories
     public interface IMqttClient
     {
         string ClientId { get; }
-
         IMqttApplicationMessageReceivedHandler MessageReceivedHandler { get; set; }
 
+        #region Connecting
         Task ConnectAsync();
-
         Task ConnectAsync(CancellationToken cancellationToken);
+        #endregion
+        #region Disconnecting
 
         Task DisconnectAsync();
-
         Task DisconnectAsync(CancellationToken cancellationToken);
-
+        #endregion
         void InitClient(ConnectionMode connectionMode, bool secureConnection);
-
-        MQTTnet.Client.IMqttClient OnConnected(Func<MQTTnet.Client.Connecting.MqttClientConnectedEventArgs, Task> handler);
-
-        MQTTnet.Client.IMqttClient OnMessageReceived(Func<MqttApplicationMessageReceivedEventArgs, Task> handler);
-
-        MQTTnet.Client.IMqttClient OnDisconnected(Func<MQTTnet.Client.Disconnecting.MqttClientDisconnectedEventArgs, Task> handler);
-
+        #region Publishing
         Task<PublishResult> PublishFeedAsync(string feedKey, string value);
-
         Task<PublishResult> PublishTopicAsync(string topic, string value);
-
+        #endregion
+        #region Subscribing
         Task<SubscribeResult> SubscribeFeedAsync(string feedKey);
-
         Task<SubscribeResult> SubscribeTopicAsync(string topic);
-
+        #endregion
+        #region Unsubscribing
         Task<UnSubscribeResult> UnSubscribeFeedAsync(params string[] feedKeys);
-
         Task<UnSubscribeResult> UnsubscribeTopicAsync(params string[] topics);
+        #endregion
+        #region OnConnected
+        public delegate void ConnectedDelegate(MQTTnet.MqttApplicationMessageReceivedEventArgs eventArgs);
+        public event ConnectedDelegate OnConnected;
+        #endregion
+        #region OnMessageReceived
+        public delegate void MessageReceivedDelegate(MQTTnet.MqttApplicationMessageReceivedEventArgs eventArgs);
+        public event MessageReceivedDelegate OnMessageReceived;
+        #endregion
+        #region OnDisconnected
+        public delegate void DisconnectedDelegate(MQTTnet.MqttApplicationMessageReceivedEventArgs eventArgs);
+        public event DisconnectedDelegate OnDisconnected;
+        #endregion
+
     }
 }
